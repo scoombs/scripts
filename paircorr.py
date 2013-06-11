facf4231ba649a4ccba1e174babb931ea813d64f
scoombs <sarah.coombs@uoit.net>
2013-06-11 11:38:15 -0400

    paircorr.py creates a list of all pos'ble atompairs

--- paircorr.py
+++ paircorr.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #Computes pair correlation function,g(r),taking *.xyz input file
 import os,sys
-import numpy as np
-import sets
+import numpy as np 
 
 def main():
     """
@@ -27,13 +26,16 @@ def main():
     
     #loop to find the distance between two atoms
     pairs = []
-    for atom1 in atoms:
-        print 'atom1 =',atom1
-        for atom2 in atoms:
-            if atom1 != atom2:     
-               pairs.append([atom1,atom2])
-               pair = set(tuple(pairs))
-               print pair 
+    for i in range(natoms): #loops over first atom
+        atom1 = atoms[i] 
+        #print 'atom1 =',atom1
+        for j in np.arange(i,natoms): #loops over a second atom,after i to avoid duplicate combos
+            atom2 = atoms[j]
+          #  print 'atom2 =' ,atom2 
+            if atom1 != atom2: #create a pair of all possible combos of 2 different atoms
+             #  print atom1,atom2     
+               pairs.append([atom1,atom2]) #stores pairs in a list called "pairs"
+               print pairs        
 
 # This executes main() only if executed from shell
 if __name__ == '__main__':
