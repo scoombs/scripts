014690d001a6a756cec48f334816884334d80650
scoombs <sarah.coombs@uoit.net>
2013-06-11 14:53:24 -0400

    first attempt at a histogram in paircorr.py

--- paircorr.py
+++ paircorr.py
@@ -7,11 +7,22 @@ def main():
     """
     This is the main function.
     """
+    #Allow the user to input a lattice constant and number of bins to be used:
+    try:
+        program =sys.argv[0] #Gives filename
+        lattice = int(sys.argv[1])
+        nbins = int(sys.argv[2])
+    except IndexError:
+        #Tell user what is needed
+        print '\nusage: '+program+' lattice nbins (where lattice & nbins are integers)\n' 
+        #Exit program cleanly
+        sys.exit(0)
+
     inputfile = open( 'test.xyz' , 'r')
     outfile = open('paircorr_data' , 'w')
-    
-    natoms = int(inputfile.readline().split()[0])  #reads line 1,number of atoms
-    inputfile.readline() # reads line 2, blank space
+   
+    natoms = int(inputfile.readline().split()[0])  #Reads line 1,number of atoms
+    inputfile.readline() #Reads line 2, blank space
   
     atoms = []
     for line in inputfile:
@@ -20,19 +31,19 @@ def main():
 #        z= line.split()[3]
 #        lists.append([x,y,z])
 
-        tmp = line.split() # splits list
-        tmp.pop(0)         # pops off first entry in each list (atom type)
-        atoms.append(tmp)  # appends lists of atom coordinates together
+        tmp = line.split() # Splits list
+        tmp.pop(0)         # Pops off first entry in each list (atom type)
+        atoms.append(tmp)  # Appends lists of atom coordinates together
     
-    #loops to find the distance between two atoms:
-    for i in range(natoms): #loops over first atom
+    #Loops to find the distance between two atoms:
+    for i in range(natoms): #Loops over first atom
         atom1 = atoms[i] 
 
-        for j in np.arange(i+1,natoms): #loops over a second atom,after i to avoid duplicate combos
+        for j in np.arange(i+1,natoms): #Loops over a second atom,after i to avoid duplicate combos
             atom2 = atoms[j]
-           # print 'atom1,atom2=', atom1,atom2  #test is good,produces (natoms choose 2) combos everytime    
+           # print 'atom1,atom2=', atom1,atom2  #Test is good,produces (natoms choose 2) combos everytime    
           
-            #Finds the difference between x,y,z coordinates of each pair
+            #Finds the difference between x,y,z coordinates of each pair:
             x_pair_diff =float(atom1[0]) - float(atom2[0])
             y_pair_diff =float(atom1[1]) - float(atom2[1])
             z_pair_diff =float(atom1[2]) - float(atom2[2])        
@@ -41,7 +52,8 @@ def main():
             distances = (x_pair_diff**2 + y_pair_diff**2 + z_pair_diff**2)**(1./2.)
            # print distances
 
-
+            hist,bin_edges = np.histogram(distances,bins=nbins, range=(0,lattice/2))
+            print hist,bin_edges
 # This executes main() only if executed from shell
 if __name__ == '__main__':
     main()
