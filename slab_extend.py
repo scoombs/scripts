41c15c051054a9dd42f9103a94a97f3d95f18fd5
scoombs <sarah.coombs@uoit.net>
2013-06-14 09:28:21 -0400

    now reads in number of each atom

--- slab_extend.py
+++ slab_extend.py
@@ -16,24 +16,30 @@ def main():
     inputfile.readline() #skips 2nd line
     
     #Retrieve lattice constants from POSCAR file:
-    line3 = inputfile.readline()
-    x_lattice = float(line3.split()[0])
-    #NOTE:if not orthonormal,could have made this into an array x_lattice= [all that stuff] 
-    line4 = inputfile.readline()
-    y_lattice = float(line4.split()[1])
-    line5 = inputfile.readline()
-    z_lattice = float(line5.split()[2])
-#    print x_lattice,y_lattice,z_lattice
- 
-    #For now, skip over the line describing number of atoms:
+   
+    x_lattice = float(inputfile.readline().split()[0]) #reads in line 3
+    #NOTE:if not orthonormal, make this into an array x_lattice= [float(line3.split()[0]] 
+   
+    y_lattice = float(inputfile.readline().split()[1]) #reads in line 4
+   
+    z_lattice = float(inputfile.readline().split()[2]) #reads in line 5
+    # print x_lattice,y_lattice,z_lattice
+  
     inputfile.readline() #skips 6th line,types of atoms
-    inputfile.readline() #skips 7th line,number of atoms
+   
+    #Read in line defining how many of each type of atom, to be used in loop later on
+    line7 = inputfile.readline() #Reads 7th line,number of atoms
+    natom1 = int(line7.split()[0]) #Reads in number of atoms type 1
+    natom2 = int(line7.split()[1]) #Reads in number of atoms type 2
+    natom3 = int(line7.split()[2]) # Reads in number of atoms type 3
+   # print natom1,natom2,natom3
+
     inputfile.readline() #skips 8th line,Direct 
    
     x_array = []
     y_array = []
     z_array = [] 
-    #Loop to convert direct to cartesian and separate into x,y,z arrays
+    #Loop to separate into x,y,z arrays and convert Direct to Cartesian
     for line in inputfile:
         if len(line.split()) > 0:
         #Retrieve columns from POSCAR (remember to remove spacing in file) 
@@ -44,7 +50,7 @@ def main():
     inputfile.close()
     
     #Convert columns to Cartesian:
-    x_cartesian =[ x * x_lattice for x in x_array] #element wise multiplication
+    x_cartesian = [x * x_lattice for x in x_array] #element wise multiplication
     y_cartesian = [y * y_lattice for y in y_array]
     z_cartesian = [z * z_lattice for z in z_array] 
    # print x_cartesian,y_cartesian,z_cartesian     
