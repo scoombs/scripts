61f5d21a1c7ef1ad6081e3902a9e2cfb1039a2c0
scoombs <sarah.coombs@uoit.net>
2013-06-14 09:15:24 -0400

    reads in atom type

--- slab_extend.py
+++ slab_extend.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-#Program to extend POSCAR inputfile, a slab, in the x and y direction, and output the new file,whihc contains only coordinates, atomtypes,etc mus tbe added by hand for now
+#Program to extend a Ti,Si,O POSCAR inputfile, a slab, in the x and y direction, and output the new file,whihc contains only coordinates, atomtypes,etc mus tbe added by hand for now
 
 import os,sys
 def main():
 
-    inputfile = open('POSCAR', 'r')
+    inputfile = open('POSCAR1', 'r')
     outputfile = open('extend_POSCAR','w')
 
-    #Want to skip over the first 2 lines of the POSCAR file
-    inputfile.readline() #skips 1st line
+    #Read in the first line of the POSCAR file:
+    line1 = inputfile.readline() 
+    atom1 = str(line1.split()[0]) #reads in atom type 1
+    atom2 = str(line1.split()[1]) #reads in atom type 2
+    atom3 = str(line1.split()[2]) #reads ina tom type 3
+   # print atom1,atom2,atom3
     inputfile.readline() #skips 2nd line
     
     #Retrieve lattice constants from POSCAR file:
@@ -64,7 +68,7 @@ def main():
    # print new_xcoordinates,new_ycoordinates     
 
     for i in range(len(new_xcoordinates)):
-        print new_xcoordinates[i], new_ycoordinates[i], z_cartesian[i%len(z_cartesian)]
+      #  print new_xcoordinates[i], new_ycoordinates[i], z_cartesian[i%len(z_cartesian)]
         outputfile.write(str(new_xcoordinates[i]) + ' ' + str(new_ycoordinates[i]) + ' ' + str(z_cartesian[i%len(z_cartesian)]) + '\n')
     
     outputfile.close()
