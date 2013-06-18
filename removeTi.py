07c251df2a4fb2e4b6db9f3957577c35678090e4
scoombs <sarah.coombs@uoit.net>
2013-06-18 14:58:30 -0400

    strips Ti from POSCAR file & outputs  just coordinates

--- removeTi.py
+++ removeTi.py
@@ -1,1 +1,26 @@
-/home/scoombs/slab2/removeTi.py
+#!/usr/bin/env python
+#Program to extract coordinates from POSCAR file and delete first atom ,Ti
+import os,sys
+def main():
+    inputfile = open('POSCAR', 'r')
+    outputfile = open('slab2','w')
+    
+    #Skips first 6 lines in POSCaR file
+    for line in range(6):
+        line = inputfile.readline()
+    #Read in total number of atoms
+    natoms = sum(map(int,inputfile.readline().split())) 
+    inputfile.readline() #skips line 8,Direct
+    
+    inputfile.readline()#skips atom 1, Ti
+
+    #Reads in coordinates and writes it into outputfile without line spacing
+    for atom in range(natoms - 1):
+       line =  inputfile.readline()
+       outputfile.write(line.strip() + '\n')
+    
+    inputfile.close()
+    outputfile.close()
+   
+if __name__=='__main__':
+    main() 
