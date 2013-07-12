1369b4defb84c8334e033461f9b809aa28f3a7a5
scoombs <sarah.coombs@uoit.net>
2013-07-12 14:57:36 -0400

    works

--- surf_bulk_seperator.py
+++ surf_bulk_seperator.py
@@ -3,40 +3,32 @@
 import os,sys
 import numpy as np
 
-def pbc_round(input_value):
-    """
-This function is used for periodic boundary conditions,it rounds a value to the nearest integer.
-
-"""
-    i = int(input_value)
-    if (abs(input_value-i) >= 0.5):
-        if (input_value > 0): i+=1
-        if (input_value < 0): i-=1
-    return i
-
 def main():
-
-    #Allow user input of suraface width:
+    
+    #Allow user input of surface width:
     try:
         program = sys.argv[0] #Gives filename
-        lattice = float(sys.argv[1])
-        width = float(sys.argv[2])
+        width = float(sys.argv[1])
     except IndexError:
         #Tell user what is needed
-        print '\n usage: '+program+' lattice width (where the units are in angstrom,1 nanometer = 10 angstrom)'
+        print '\n usage: '+program+' width (where the units are in angstrom,1 nanometer = 10 angstrom)'
         sys.exit(0)
- 
-    inputfile = open('quartz324.xyz','r')
+
+    #Allow user to input file name from terminal:
+    filein = raw_input("Enter filename (xyz format):")
+    inputfile = open(filein, 'r')
+
+   # inputfile = open('quartz324.xyz','r')
     surf_outputfile = open('surf.xyz','w')
     bulk_outputfile = open('bulk.xyz','w')
    
     natoms = int(inputfile.readline().strip()) #Reads in number of atoms
     inputfile.readline() #Skips line 2 (blank/comment line)
-      atoms = []
+    atoms = []
     for i in range(natoms):
         line = inputfile.readline()
         atoms.append(line.split()) # Appends lists of atom coordinates together
- 
+   # print atoms
     inputfile.close()
 
     #Convert elements to floats instead of strings: (Note: each 'row' is a set of coordinates for an atom: (TYPE,x,y,z))
@@ -45,18 +37,23 @@ def main():
             row[i] = float(row[i])
 
     #First find the maximum z value:
+    z = []
     for i in range(natoms):
         atom = atoms[i]
         z.append(float(atom[3]))
     max_z = max(z)
-    
-    #Loop over atoms & write out only those where z-coordinate  max - width < z < max
+    print max_z
+    #Loop over atoms & seperate them based on the "width" of surface given
     for i in range(natoms):
         atom = atoms[i]
         z_diff = abs(float((atom[3] - max_z)))
-        if z_diff <= width
+        if z_diff <= width:
             surf_outputfile.write(str(atom[0]) + ' '+ str(atom[1]) + ' ' + str(atom[2]) + ' ' + str(atom[3]) + '\n')
+        if z_diff > width:
+            bulk_outputfile.write(str(atom[0]) + ' '+ str(atom[1]) + ' ' + str(atom[2]) + ' ' + str(atom[3]) + '\n')
+
     surf_outputfile.close()
+    bulk_outputfile.close()
 
 
 if __name__=='__main__':
