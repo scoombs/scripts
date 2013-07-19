c54afa75b5c7673f05ac7b260adcd8d20acc55ef
scoombs <sarah.coombs@uoit.net>
2013-07-19 14:14:47 -0400

    finds distance nd array for Si

---
+++ surfbulk_nn.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#Program reads in an SiO2 xyz file & calculates number of nearest neighbours in surface Si & bulk Si atoms
+
+import os,sys
+import numpy as np
+import scipy
+
+def pbc_round(input_value):
+    """
+This function is used for periodic boundary conditions,it rounds a value to the nearest integer.
+
+"""
+    i = int(input_value)
+    if (abs(input_value-i) >= 0.5):
+        if (input_value > 0): i+=1
+        if (input_value < 0): i-=1
+    return i
+
+def main():
+    
+     #Allow the user to input the periodic boundary conditions (box size) and number of timesteps to be used:
+    try:
+        program =sys.argv[0] #Gives filename
+        lattice = float(sys.argv[1])
+        lattice_x = lattice#float(sys.argv[2])
+        lattice_y = lattice#float(sys.argv[3])
+        lattice_z = float(sys.argv[2])
+        first_minimum = float(sys.argv[3])
+        nsteps = int(sys.argv[4])
+     
+    except IndexError:
+        #Tell user what is needed
+        print '\nusage: '+program+' lattice z-lattice first_gr_minimum nsteps (where lattice & nsteps & nbins are floats)\n'
+        #Exit program cleanly
+        sys.exit(0)
+
+    #Allow user to input file name from terminal:
+    filein = raw_input("Enter filename (xyz format):")
+    inputfile = open(filein, 'r')
+    
+    
+    distances = []
+    for n in range(nsteps): #Time step loop
+
+        natoms = int(inputfile.readline().strip()) #Reads in number of atoms
+        inputfile.readline() #Reads line2, blank space
+
+        atoms = []
+        for i in range(natoms):
+            line = inputfile.readline()
+            atoms.append(line.split()) #Appends lists of atom coordinates [TYPE,x,y,z]
+        #To loop over only Si:
+        for i in range(natoms/3):
+            atom1 = atoms[i]
+
+            for j in range(natoms):#Loops over second atom,doesn't account for duplicates
+                atom2 = atoms[j]
+          
+                #Finds the difference between x,y,z coordinates of each pair:
+                x_pair_diff = float(atom1[1]) - float(atom2[1])
+                y_pair_diff = float(atom1[2]) - float(atom2[2])
+                z_pair_diff = float(atom1[3]) - float(atom2[3])
+            
+                #Need to consider affects of periodic boundary conditions:
+                x_pair_diff -= lattice_x*pbc_round(x_pair_diff/lattice_x)
+                y_pair_diff -= lattice_y*pbc_round(y_pair_diff/lattice_y)
+                z_pair_diff -= lattice_z*pbc_round(z_pair_diff/lattice_z)
+   
+                distances.append((x_pair_diff**2 + y_pair_diff**2 + z_pair_diff**2)**(1./2.))
+    distances = scipy.array(distances) #Creates scipy array
+    distances = scipy.reshape(distances,(nsteps*natoms/3,-1)) 
+    #print distances
+    
+
+
+if __name__=='__main__':
+     main()
